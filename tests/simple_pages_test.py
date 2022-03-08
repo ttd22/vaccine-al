3fccb00cbace58b396768638d6a3d43b7f215d63
trangttdang <trangttdang01@gmail.com>
2022-03-07 23:42:34 -0500

    updated base.html and tests

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -6,34 +6,33 @@ def test_request_main_menu_links(client):
     assert response.status_code == 200
     assert b'<a class="nav-link" href="/git">Git</a>' in response.data
     assert b'<a class="nav-link" href="/docker">Docker</a>' in response.data
-    assert b'<a class="nav-link" href="/python-flask">Python-Flask</a>' in response.data
+    assert b'<a class="nav-link" href="/pythonflask">Python-Flask</a>' in response.data
     assert b'<a class="nav-link" href="/cicd">CICD</a>' in response.data
 
 def test_request_index(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Index Page" in response.data
 
-def test_request_about(client):
+def test_request_git(client):
     """This makes the index page"""
     response = client.get("/git")
     assert response.status_code == 200
     assert b"Git" in response.data
 
-def test_request_page1(client):
+def test_request_docker(client):
     """This makes the index page"""
     response = client.get("/docker")
     assert response.status_code == 200
     assert b"Docker" in response.data
 
-def test_request_page2(client):
+def test_request_pythonflask(client):
     """This makes the index page"""
     response = client.get("/python-flask")
     assert response.status_code == 200
     assert b"Python-Flask" in response.data
 
-def test_request_page3(client):
+def test_request_cicd(client):
     """This makes the index page"""
     response = client.get("/cicd")
     assert response.status_code == 200
